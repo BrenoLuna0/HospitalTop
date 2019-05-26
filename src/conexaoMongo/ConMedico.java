ba6a4632b456bbd4acb3175307e1c53f3d77ad1f
filipetabosas@gmail.com <Fillyp 13>
2019-05-26 20:34:58 -0300

    Otimização de ConMedico

--- src/conexaoMongo/ConMedico.java
+++ src/conexaoMongo/ConMedico.java
@@ -2,6 +2,7 @@ package conexaoMongo;
 
 import java.util.ArrayList;
 
+import classesBase.Constantes;
 import classesBase.Medico;
 import com.google.gson.Gson;
 import com.mongodb.*;
@@ -10,51 +11,48 @@ import com.mongodb.client.*;
 import org.bson.Document;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
 
-
-@SuppressWarnings("Duplicates")
+@SuppressWarnings({"Duplicates", "LoopStatementThatDoesntLoop"})
 public class ConMedico{
 
-	MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-	MongoDatabase db = mongoClient.getDatabase("hospitalTop");
-	MongoCollection<Document> collection = db.getCollection("medico");
+    private MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://"+Constantes.BD_ADDRESS.getValor()+":"+Constantes.BD_PORT.getValor()));
+	private MongoDatabase db = mongoClient.getDatabase(Constantes.BD_NAME.getValor());
+	private MongoCollection<Document> collection = db.getCollection("medico");
 
     /**
      * Insere o Medico no BD
      * */
 	public void inserir(Medico m) {
 
-        Document doc = Document.parse(m.toJson());
-		collection.insertOne(doc);
+		collection.insertOne(montarDocument(m));
 	}
 
     /**
      * Remove o Medico do BD
-     * */
+     **/
 	public void remover(Medico m) {
-        Document doc = Document.parse(m.toJson());
-        collection.deleteOne(doc);
+
+        collection.deleteOne(montarDocument(m));
 	}
 
     /**
-     * Remove o Medico do BD
-     * */
+     * Atualizar o Medico no BD
+     **/
 	public void atualizar(Medico antigo, Medico novo) {
 
-        collection.replaceOne(Document.parse(antigo.toJson()), Document.parse(novo.toJson()));
+        collection.replaceOne(montarDocument(antigo), montarDocument(novo));
 	}
 
     /**
-     * Busca de Medico pelo _id (ObjectId Mongo)
+     * Busca o Medico pelo _id (ObjectId Mongo)
      * */
-	public Medico buscarPorId(int _id) {
+	public Medico buscarPorId(String _id) {
         FindIterable<Document> documents = collection.find(eq("_id", _id));
 
         for (Document d: documents) {
-            Medico m = new Gson().fromJson(d.toJson(), Medico.class);
-            return m;
+            return montarObjeto(d.toJson());
         }
-
 		return null;
 	}
 
@@ -65,8 +63,7 @@ public class ConMedico{
         FindIterable<Document> documents = collection.find(eq("crm", crm));
 
         for (Document d: documents) {
-            Medico m = new Gson().fromJson(d.toJson(), Medico.class);
-            return m;
+            return montarObjeto(d.toJson());
         }
         return null;
 	}
@@ -75,16 +72,9 @@ public class ConMedico{
      * Busca de Medico pelo nome
      * */
 	public ArrayList<Medico> buscarPorNome(String nome) {
-        ArrayList<Medico> medicos = new ArrayList<>();
 
-        FindIterable<Document> documents = collection.find(eq("nome", nome));
-
-        for (Document d: documents) {
-            Medico m = new Gson().fromJson(d.toJson(), Medico.class);
-            medicos.add(m);
-        }
-
-        return medicos;
+        MongoCursor<Document> documents = collection.find(regex("nome", nome)).iterator();
+        return montarArrayList(documents);
 	}
 
 	/**
@@ -93,20 +83,38 @@ public class ConMedico{
      * @return ArrayList<Medico> contendo os médicos encontrados no BD (null se não tiver nenhum Medico)
      * */
 	public ArrayList<Medico> listar() {
-	    ArrayList<Medico> medicos = new ArrayList<>();
 
         MongoCursor<Document> cursor = collection.find().iterator();
+        return montarArrayList(cursor);
+	}
+
+	/**
+     * Recebe um objeto Medico e retorna o documento correspondente
+     **/
+	private Document montarDocument(Medico m){
+        return Document.parse(m.toJson());
+    }
+
+    /**
+     * Recebe um Document e retorna o objeto correspondente
+     **/
+    private Medico montarObjeto(String json){
+	    return new Gson().fromJson(json, Medico.class);
+    }
+
+    /**
+     * Recebe um cursor Document e retorna o ArrayList correspondente
+     **/
+    private ArrayList<Medico> montarArrayList(MongoCursor<Document> cursor){
+
+        ArrayList<Medico> medicos = new ArrayList<>();
         try {
             while (cursor.hasNext()) {
-                Gson gson = new Gson();
-                Medico m = gson.fromJson(cursor.next().toJson(), Medico.class);
-                medicos.add(m);
+                medicos.add(montarObjeto(cursor.next().toJson()));
             }
         } finally {
             cursor.close();
         }
-
         return medicos;
-	}
-
+    }
 }
